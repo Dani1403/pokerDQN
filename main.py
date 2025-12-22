2e6eaf1e335c13a240b7d7c53f86ca275074e3e0
danis <danis@LAPTOP-CEB13IK6>
2025-12-22 15:25:05 +0200

    full self play with 4 different dqn learning and eval

--- main.py
+++ main.py
@@ -17,12 +17,12 @@ def main():
     dqn4 = DQNAgent(env, name="dqn4")
 
     SELF_PLAY_LINEUP = [dqn1, dqn2, dqn3, dqn4]
-    N_total = 1_000
-    learn_size = 200
+    N_total = 20_000
+    learn_size = 5_000
 
     checkpoint_dirs = train_and_save(env, N_total=N_total, learn_size=learn_size, training_lineup=SELF_PLAY_LINEUP, checkpoint_root="checkpoints")
     env.close()
-    eval_checkpoint_dir(checkpoint_dirs=checkpoint_dirs, n_workers_per_lineup=2, n_tournaments_per_worker=200)
+    eval_checkpoint_dir(checkpoint_dirs=checkpoint_dirs, n_workers_per_lineup=6, n_tournaments_per_worker=400)
 
 
 if __name__ == "__main__":
