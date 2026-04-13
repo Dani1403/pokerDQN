21044480358b590ad7364495bfe1faaa0c7b4db1
danis <danis@LAPTOP-CEB13IK6>
2026-04-13 10:41:45 +0300

    training 100k

--- main.py
+++ main.py
@@ -42,19 +42,20 @@ def main():
     RANDOM_LINEUP = [poker_dqn_1, RandomAllInFoldAgent(env), RandomAllInFoldAgent(env), RandomAllInFoldAgent(env)]
     ALL_IN_PAIR_LINEUP = [poker_dqn_1, AllInPairAgent(
         env), AllInPairAgent(env), AllInPairAgent(env)]
-    N_total = 20_000
-    learn_size = 5_000
-    #checkpoint_dir = train_and_save(env, N_total=N_total, learn_size=learn_size,training_lineup=SELF_PLAY_LINEUP, checkpoint_root="checkpoints")
-    checkpoint_dirs = {
-        "poker_dqn_1": "checkpoints/poker_dqn_1_20260107_173558_609746",
-        "poker_dqn_2": "checkpoints/poker_dqn_2_20260107_173558_609746",
-        "poker_dqn_3": "checkpoints/poker_dqn_3_20260107_173558_609746",
-        "poker_dqn_4": "checkpoints/poker_dqn_4_20260107_173558_609746",
-    }
+    N_total = 1_000_000
+    learn_size = 50_000
+    checkpoint_dirs = train_and_save(env, N_total=N_total, learn_size=learn_size,training_lineup=SELF_PLAY_LINEUP, checkpoint_root="checkpoints")
+
+    # checkpoint_dirs = {
+    #     "poker_dqn_1": "checkpoints/poker_dqn_1_20260107_173558_609746",
+    #     "poker_dqn_2": "checkpoints/poker_dqn_2_20260107_173558_609746",
+    #     "poker_dqn_3": "checkpoints/poker_dqn_3_20260107_173558_609746",
+    #     "poker_dqn_4": "checkpoints/poker_dqn_4_20260107_173558_609746",
+    # }
     env.close()
     
     eval_checkpoint_dir(checkpoint_dirs=checkpoint_dirs,
-                            n_workers_per_lineup=4, n_tournaments_per_worker=1_000)
+                            n_workers_per_lineup=10, n_tournaments_per_worker=10_000)
 
 if __name__ == "__main__":
     #pr = cProfile.Profile()
